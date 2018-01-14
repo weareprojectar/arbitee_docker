fbca55039aecd2a2ad9ef8d3f01cff4e495d69cd
Project AR <weareprojectar@gmail.com>
2018-01-15 01:06:54 +0900

    added docker builds in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
+
+
 # install Docker on server
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 apt-get update
-
 apt-get install docker-ce
-
 apt-get install docker-compose
 
+# create necessary images
+docker build --tag node-nginx:test .
+cd nginx
+docker build --tag node-nginx-lb:test .
+cd ../
 
 # run docker-compose up
-
 docker-compose up -d
