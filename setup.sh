547aa73ebe61a0f32c4ca1f555f87de5d27b9512
Project AR <weareprojectar@gmail.com>
2018-01-15 00:56:34 +0900

    added setup bash script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# install Docker on server
+
+curl -fsSL https://download.docker.com/linux.ubuntu.gpg | sudo apt-key add -
+
+add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get update
+
+apt-get install docker-ce
+
+apt-get install docker-compose
+
+
+# run docker-compose up
+
+docker-compose up -d
