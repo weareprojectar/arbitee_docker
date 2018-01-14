3d703ff8f6b7cdfe9b5a2e620413979a858638b1
Project AR <weareprojectar@gmail.com>
2018-01-15 01:01:24 +0900

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # install Docker on server
 
-curl -fsSL https://download.docker.com/linux.ubuntu.gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
