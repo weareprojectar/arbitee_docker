ee500e86919483d6f304f96b4d215b725fb8744b
wonseok0403 <wonseok0403@naver.com>
2018-01-15 15:29:04 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,29 +4,16 @@
 # @date    2017/1/15
 # @details finding better way (now)
 
-# install Docker on server
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
-apt-get install docker-compose
 
+# update images
+docker-compose up -d --no-deps --build nginx
+docker-compose up -d --no-deps --build app
 
-#remove all containers - by wonseok (2017/01/15)
-docker rm -f $(docker ps -a -q)
-for f in `docker images | grep -v IMAGE | awk '{split($0,array," ")} {print array[3]}'`
-do 
- echo "==> delete image : $f"
- docker rmi $f 
-done
-
-
-# create necessary images
+# re-build images
 docker build --tag node-nginx:test .
 cd nginx
 docker build --tag node-nginx-lb:test .
 cd ../
 
-# run docker-compose up
+# run compose up
 docker-compose up -d
