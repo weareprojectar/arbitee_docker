8fd286f888aa7c9f401802006872476a70d822ef
yorong <tpgml6824@gmail.com>
2018-01-17 19:05:01 +0900

    index.js setting for connect html file

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var express = require('express')
-var uuid = require('uuid')
 
-var app = express()
-var id = uuid.v4()
-var port = 3000
+var express = require('express');
+var uuid = require('uuid');
 
-app.get('/', function (req, res) {
-  res.send(id)
-})
+var app = express();
+var id = uuid.v4();
+var port = 3000;
+
+app.use(express.static(__dirname));
+
+
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', { root : __dirname });
+});
 
 app.listen(port, function () {
-  console.log('Example app listening on port: ' + port)
-})
+  console.log('Example app listening on port: ' + port);
+});
